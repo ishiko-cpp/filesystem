f4143fdc4cf72c0433d75b91d744e33ba82db7dd
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-01-31 21:49:21 +0000

    Add files

---
+++ Include/Ishiko/FileSystem/Directory.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/FileSystem/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_FILESYSTEM_DIRECTORY_H_
+#define _ISHIKO_FILESYSTEM_DIRECTORY_H_
+
+#endif
---
+++ Source/Directory.cpp
@@ -0,0 +1,7 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/FileSystem/blob/master/LICENSE.txt
+*/
+
+#include "Directory.h"
