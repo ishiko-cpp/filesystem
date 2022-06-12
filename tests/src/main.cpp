609d357ad98696ce708d2567df5f93f77faba47b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-12 17:29:20 +0100

    Update test code after changes to dependencies (#76)

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -16,9 +16,9 @@ int main(int argc, char* argv[])
 {
     TestHarness theTestHarness("IshikoFileSystem");
 
-    theTestHarness.context().setTestDataDirectory("../../data");
-    theTestHarness.context().setTestOutputDirectory("../../output");
-    theTestHarness.context().setReferenceDataDirectory("../../reference");
+    theTestHarness.context().setDataDirectory("../../data");
+    theTestHarness.context().setOutputDirectory("../../output");
+    theTestHarness.context().setReferenceDirectory("../../reference");
 
     TestSequence& theTests = theTestHarness.tests();
     theTests.append<BinaryFileTests>();
